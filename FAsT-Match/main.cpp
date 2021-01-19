f1dad60441fb84e53511fedef036fc42ed052069
yumetodo <yume-wikijp@live.jp>
2021-01-19 12:54:13 +0900

    fix: cvFontQt is old name. use fontQt instead.

--- FAsT-Match/main.cpp
+++ FAsT-Match/main.cpp
@@ -17,9 +17,12 @@ using namespace cv;
 
 int main(int argc, const char * argv[])
 {
-
-    Mat image = imread( "/Users/saburookita/Personal Projects/FAsT-Match/image.png" );
-    Mat templ = imread( "/Users/saburookita/Personal Projects/FAsT-Match/template.png" );
+    if (argc != 3) {
+        std::cerr << "FAsTMatch [image] [template]" << std::endl;
+        return 1;
+    }
+    Mat image = imread( argv[1] );
+    Mat templ = imread( argv[2] );
     
     fast_match::FAsTMatch fast_match;
     fast_match.init( 0.15f, 0.85f, false, 0.5f, 2.0f );
@@ -36,7 +39,7 @@ int main(int argc, const char * argv[])
     
     Mat appended( image.rows, 2 * image.cols, CV_8UC3, Scalar(0, 0, 0) );
     
-    CvFont font = cvFontQt("Helvetica", 14.0, CV_RGB(0, 255, 0) );
+    auto font = fontQt("Helvetica", 14.0, CV_RGB(0, 255, 0) );
     addText( appended, "Template: ", Point( 50, 50 ), font );
     templ.copyTo( Mat(appended, Rect((image.cols - templ.cols) / 2, (image.rows - templ.cols) / 2, templ.cols, templ.rows)) );
     image.copyTo( Mat(appended, Rect( image.cols, 0, image.cols, image.rows)) );
